ef43854cd68061cb4dea02016b4ad58f1c0bdebf
18829489477 <1226864411@qq.com>
2018-06-22 23:42:27 +0800

    统计数据

--- src/main/java/io/openmessaging/DefaultQueueStoreImpl.java
+++ src/main/java/io/openmessaging/DefaultQueueStoreImpl.java
@@ -1,9 +1,6 @@
 package io.openmessaging;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
 
 /**
@@ -12,21 +9,52 @@ import java.util.concurrent.ConcurrentHashMap;
  */
 public class DefaultQueueStoreImpl extends QueueStore {
 
-
     public static Collection<byte[]> EMPTY = new ArrayList<byte[]>();
     Map<String, List<byte[]>> queueMap = new ConcurrentHashMap<String, List<byte[]>>();
+    Set<String> duplication = new HashSet();
 
     public synchronized void put(String queueName, byte[] message) {
+
         if (!queueMap.containsKey(queueName)) {
-            queueMap.put(queueName, new ArrayList<byte[]>());
+            queueMap.put(queueName, new ArrayList<byte[]>(100));
         }
         queueMap.get(queueName).add(message);
+
+        String name =new StringBuilder().append(queueName).append("-").append(Thread.currentThread().getId()).toString();
+        if(!duplication.contains(name)){
+            System.out.println(new StringBuilder().append(Thread.currentThread().getId()).append( "--> ")
+                    .append(queueName).append(":")
+                    .append(message.length).append(":").append(queueMap.get(queueName)).toString());
+            duplication.add(name);
+        }
+        if(message.length > 64){
+            System.out.println("size > 64");
+        }
     }
     public synchronized Collection<byte[]> get(String queueName, long offset, long num) {
+        System.out.println(new StringBuilder().append(Thread.currentThread().getId()).append( "--> ")
+                .append(queueName).append(":").append(offset).append(":").append(num));
         if (!queueMap.containsKey(queueName)) {
             return EMPTY;
         }
         List<byte[]> msgs = queueMap.get(queueName);
         return msgs.subList((int) offset, offset + num > msgs.size() ? msgs.size() : (int) (offset + num));
     }
+
+//    public static Collection<byte[]> EMPTY = new ArrayList<byte[]>();
+//    Map<String, List<byte[]>> queueMap = new ConcurrentHashMap<String, List<byte[]>>();
+//
+//    public synchronized void put(String queueName, byte[] message) {
+//        if (!queueMap.containsKey(queueName)) {
+//            queueMap.put(queueName, new ArrayList<byte[]>());
+//        }
+//        queueMap.get(queueName).add(message);
+//    }
+//    public synchronized Collection<byte[]> get(String queueName, long offset, long num) {
+//        if (!queueMap.containsKey(queueName)) {
+//            return EMPTY;
+//        }
+//        List<byte[]> msgs = queueMap.get(queueName);
+//        return msgs.subList((int) offset, offset + num > msgs.size() ? msgs.size() : (int) (offset + num));
+//    }
 }
